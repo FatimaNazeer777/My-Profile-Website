6fdaaf592035d3e150b0188e587581d6241b17be
Fatima Nazeer <fatimanazeer488@gmail.com>
2024-12-27 19:55:07 +0000

    Update layout to prevent horizontal overflow and adjust chatbot component placement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,18 +17,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}> 
+       <body className={`${inter.className} overflow-x-hidden`}> 
         <NavBar />
         {children}
         {/* <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1" async></script>
-<df-messenger
-  chat-icon="https:&#x2F;&#x2F;upload.wikimedia.org&#x2F;wikipedia&#x2F;commons&#x2F;0&#x2F;04&#x2F;ChatGPT_logo.svg"
-  intent="WELCOME"
-  chat-title="portfolio-chatbot"
-  agent-id="06911b46-c7dc-49fc-8747-349a9c1bd289"
-  language-code="en"
-></df-messenger> */}
-        </body>
+    <df-messenger
+      chat-icon="https:&#x2F;&#x2F;upload.wikimedia.org&#x2F;wikipedia&#x2F;commons&#x2F;0&#x2F;04&#x2F;ChatGPT_logo.svg"
+      intent="WELCOME"
+      chat-title="portfolio-chatbot"
+      agent-id="06911b46-c7dc-49fc-8747-349a9c1bd289"
+      language-code="en"
+    ></df-messenger> */}
+      </body>
     </html>
   );
 }
